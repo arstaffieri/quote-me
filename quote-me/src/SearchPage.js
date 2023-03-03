6ef1f90495e4f6e60ac679f49e3199c196db8b80
Angie Staffieri <angie.staffieri@gmail.com>
2023-03-03 09:17:25 -0700

    Search partially working

---
+++ quote-me/src/SearchPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from './Home'
+import Search from "./Search";
+
+const SearchPage = () => {
+    return (
+        <div>
+            <Home />
+            <Search />
+        </div>
+    )
+}
+
+export default SearchPage
